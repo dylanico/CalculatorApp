e659579e2b42989061d230db635398c3690cf02f
Dylan Forde <dylan.z.forde@gmail.com>
2022-04-26 13:00:58 -0400

    Allows Calculator to be used on lower versions of Xcode

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberButtonTitle: String = ""
+    var operationButtonTitle: String = ""
+    
+    
+    @IBOutlet weak var calculatorField: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func numberButton(_ sender: UIButton) {
+        
+        numberButtonTitle = sender.titleLabel?.text ?? ""
+        print(numberButtonTitle)
+        
+        updateCalculatorView()
+        
+    }
+    
+    @IBAction func operationButton(_ sender: UIButton) {
+        
+        operationButtonTitle = sender.titleLabel?.text ?? ""
+        print(operationButtonTitle)
+        
+        updateCalculatorView()
+        
+    }
+    
+    
+    
+    func updateCalculatorView(){
+        
+        
+        if calculatorField.text == "0"{
+            calculatorField.text = numberButtonTitle
+        }else{
+            calculatorField.text! += numberButtonTitle
+        }
+        
+        if operationButtonTitle == "AC"{
+            calculatorField.text = "0"
+        }
+        
+    }
 }
-
